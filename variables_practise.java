2305364627b71afa2883bbd654ecc5efe9b4853d
neshimbekova <neshimbekova@gmail.com>
2019-03-10 20:34:07 -0500

    practise file added

---
+++ variables_practise.java
@@ -0,0 +1,19 @@
+// This little code is just to learn how to use variables
+
+public class Pancake {
+	public static void main(String[] args) {
+		//PAncakes: eggs -2, flour - 1cup, milk - 0.5
+		// sugar - 2spoons, oil - 2 table spoons
+
+		int eggs = 2;
+		byte flour = 1;
+		double cupOfmilk = 0.5;
+		int sugar = 2;
+		int oil = 2;
+
+		System.out.println("To make a pancakes for two people You will need " + eggs + " eggs" +
+							flour + " cup of flour " + cupOfmilk + " cup of milk " +
+							sugar + " spoons of sugar " + oil + " table spoons of oil ");
+
+	}
+}
